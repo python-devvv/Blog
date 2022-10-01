61cf3cb83934203f4137f1c48503f51ed2c90087
Bharathan M <67325821+mbharathan@users.noreply.github.com>
2022-10-01 14:32:11 +0530

    Update urls.py

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -7,15 +7,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('edit-profile/', edit_profile, name='edit-profile'),
-    path('bookmarks/', my_bookmarks, name='bookmarks'),
-    path('top-authors/', top_authors, name='top-authors'),
-    path('feedback/', feedback, name='feedback'),
     path('', include('blog.urls')),
-    path('profile/', include('users.urls')),
+    path('', include('users.urls')),
     
 ]
 
